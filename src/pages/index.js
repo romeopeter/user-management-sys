6b9c0ef386653023321e77858a5406c9d9082f9d
romeo <romeomeshack34@gmail.com>
2023-12-21 22:12:16 +0100

    Mock user database

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,237 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { Fragment, useState, useEffect, useRef } from "react";
+import { generateRandomId } from "@/utils/randomId";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home({ user }) {
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+  const [userState, setUserState] = useState({
+    name: "",
+    email: "",
+    role: "",
+  });
+
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+
+    setUserState((user) => {
+      return {
+        ...user,
+        [name]: value,
+      };
+    });
+  };
+
+  // Get users
+  useEffect(
+    () => {
+      (async () => {
+        const response = await fetch("/api/users");
+        const user = await response.json();
+
+        setLoading(false);
+        setData(user);
+      })();
+    },
+    [],
+    [data],
+  );
+
+
+  // Post users
+  const postUser = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("/api/postUser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: generateRandomId(),
+        name: userState.name,
+        email: userState.email,
+        role: userState.role,
+      }),
+    });
+
+    const user = await response.json();
+
+    setData(user);
+    // setLoading(false);
+
+    console.log(data);
+  };
+
+  // Update user
+  const updateUser = async (e, updatedData) => {
+    e.preventDefault();
+
+    const { userId, name, email, role } = updatedData;
+
+    console.log(updatedData)
+
+    // const response = await fetch(`/api/updateUser/${userId}`, {
+    //   method: "PUT",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     name: name,
+    //     email: email,
+    //     role: role,
+    //   }),
+    // });
+
+    // const user = await response.json();
+
+    // setData(user);
+  };
+
+  // Delete user
+  const deleteUser = async (e, userId) => {
+    e.preventDefault();
+
+    const response = await fetch(`/api/updateUser/${userId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const user = await response.json();
+
+    setData(user);
+  };
+
+  // if (isLoading) return <p>Loading...</p>;
+  if (!data) return <p>No profile data</p>;
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className={`flex  h-[30rem] flex-col p-24 ${inter.className}`}>
+      <div className="z-10 max-w-5xl w-full items-center justify-between">
+        <p className="">User Management System</p>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div className="mb-32 text-center px-5 py-4">
+        <form className="mb-10">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={userState.name}
+            onChange={(e) => changeHandler(e)}
+            className="border border-black rounded p-1 w-full"
+          />{" "}
+          <br />
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={userState.email}
+            onChange={(e) => changeHandler(e)}
+            className="border border-black rounded p-1 my-2 w-full"
+          />{" "}
+          <br />
+          <input
+            type="text"
+            placeholder="Role"
+            name="role"
+            value={userState.role}
+            onChange={(e) => changeHandler(e)}
+            className="border border-black rounded p-1 w-full"
+          />
+          <input
+            type="submit"
+            className="border border-black rounded p-1 block w-full my-2 cursor-pointer"
+            onClick={(e) => postUser(e)}
+          />
+        </form>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <table class="table-auto w-full border-collapse text-sm">
+          <thead>
+            <tr>
+              <th className="border border-slate-600">Name</th>
+              <th className="border border-slate-600">Email</th>
+              <th className="border border-slate-600">Role</th>
+              <th className="border border-slate-600">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.length > 0 &&
+              data.map((data, idx) => (
+                <Fragment key={data.id}>
+                 {!data.email ? null : ( <tr>
+                    <td
+                      className="border border-slate-100 p-4 text-slate-500 dark:text-slate-400 text-sm"
+                      contentEditable
+                      onInput={(e) =>
+                        setUserState((data) => ({
+                          ...data,
+                          name: e.target.innerText,
+                        }))
+                      }
+                    >
+                      {data.name}
+                    </td>
+                    <td
+                      className="border border-slate-100 p-4 text-slate-500 dark:text-slate-400 text-sm"
+                      contentEditable
+                      onInput={(e) =>
+                        setUserState((data) => ({
+                          ...data,
+                          email: e.target.innerText,
+                        }))
+                      }
+                    >
+                      {data.email}
+                    </td>
+                    <td
+                      className="border border-slate-100 p-4 text-slate-500 dark:text-slate-400 text-sm"
+                      contentEditable
+                      onInput={(e) =>
+                        setUserState(() => ({
+                          ...data,
+                          role: e.target.innerText,
+                        }))
+                      }
+                    >
+                      {data.role}
+                    </td>
+                    <td
+                      className="border border-slate-100 p-4 text-slate-500 dark:text-slate-400 text-sm"
+                      contentEditable
+                    >
+                      <button
+                        className="text-red-300"
+                        onClick={(e) => deleteUser(e, data.id)}
+                      >
+                        Delete
+                      </button>{" "}
+                      {" | "}
+                      <button
+                        className="text-green-400"
+                        onClick={(e) =>
+                          updateUser(e, {
+                            userId: data.id,
+                            name: data.name,
+                            email: data.email,
+                            role: data.role,
+                          })
+                        }
+                      >
+                        Update
+                      </button>
+                    </td>
+                  </tr>)}
+                </Fragment>
+              ))}
+          </tbody>
+        </table>
       </div>
     </main>
-  )
+  );
 }
