9f5bda5c4a127db601f258737526fcfc6469b427
romeo <romeomeshack34@gmail.com>
2023-12-21 22:10:21 +0100

    Database

---
+++ src/userDataStore.js
@@ -0,0 +1,20 @@
+export const users = [
+  {
+    id: String(1),
+    name: "Romeo",
+    email: "romeo@gmail.com",
+    role: "developer",
+  },
+  {
+    id: String(2),
+    name: "John",
+    email: "john@gmail.com",
+    role: "designer",
+  },
+  {
+    id: String(3),
+    name: "Mather",
+    email: "mather@gmail.com",
+    role: "project manager",
+  }
+];
