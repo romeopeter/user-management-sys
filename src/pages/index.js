83d11232207ae5e5c7bee17850a60be4a8f4da6b
romeo <romeomeshack34@gmail.com>
2023-12-22 08:41:36 +0100

    fixed bug with updateUser method

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,25 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import { Fragment, useState, useEffect, useRef } from "react";
 import { generateRandomId } from "@/utils/randomId";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const userFields = {
+  name: "",
+  email: "",
+  role: "",
+}
+
 export default function Home({ user }) {
+  const [form, setForm] = useState(userFields);
+  const [updatedData, setUpdatedData] = useState(userFields)
   const [data, setData] = useState(null);
   const [isLoading, setLoading] = useState(true);
-  const [userState, setUserState] = useState({
-    name: "",
-    email: "",
-    role: "",
-  });
 
   const changeHandler = (e) => {
     const { name, value } = e.target;
 
-    setUserState((user) => {
+    setForm((user) => {
       return {
         ...user,
         [name]: value,
@@ -52,9 +54,9 @@ export default function Home({ user }) {
       },
       body: JSON.stringify({
         id: generateRandomId(),
-        name: userState.name,
-        email: userState.email,
-        role: userState.role,
+        name: form.name,
+        email: form.email,
+        role: form.role,
       }),
     });
 
@@ -62,8 +64,6 @@ export default function Home({ user }) {
 
     setData(user);
     // setLoading(false);
-
-    console.log(data);
   };
 
   // Update user
@@ -72,30 +72,35 @@ export default function Home({ user }) {
 
     const { userId, name, email, role } = updatedData;
 
-    console.log(updatedData)
+    const response = await fetch(`/api/updateUser/${userId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        role: role,
+      }),
+    });
 
-    // const response = await fetch(`/api/updateUser/${userId}`, {
-    //   method: "PUT",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify({
-    //     name: name,
-    //     email: email,
-    //     role: role,
-    //   }),
-    // });
+    const user = await response.json();
 
-    // const user = await response.json();
+    if (user.status !== 400)  {
+      console.log(user.error)
+      return
+    };
 
-    // setData(user);
+    setData(user)
+
+   
   };
 
   // Delete user
   const deleteUser = async (e, userId) => {
     e.preventDefault();
 
-    const response = await fetch(`/api/updateUser/${userId}`, {
+    const response = await fetch(`/api/deleteUser/${userId}`, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
@@ -107,22 +112,24 @@ export default function Home({ user }) {
     setData(user);
   };
 
-  // if (isLoading) return <p>Loading...</p>;
-  if (!data) return <p>No profile data</p>;
+  if (isLoading) return <p>Loading...</p>;
+  if (!data) return <p>No data</p>;
 
   return (
-    <main className={`flex  h-[30rem] flex-col p-24 ${inter.className}`}>
+    <main className={`flex  h-screen flex-col items-center p-24 ${inter.className}`}>
       <div className="z-10 max-w-5xl w-full items-center justify-between">
-        <p className="">User Management System</p>
+        <p className="text-center text-2xl">User Management System</p>
       </div>
 
-      <div className="mb-32 text-center px-5 py-4">
-        <form className="mb-10">
+      <div className="text-center px-5 py-4 max-w-3xl">
+
+        {/* Input form */}
+        <form className="mb-10 w-full">
           <input
             type="text"
             placeholder="Name"
             name="name"
-            value={userState.name}
+            value={form.name}
             onChange={(e) => changeHandler(e)}
             className="border border-black rounded p-1 w-full"
           />{" "}
@@ -131,7 +138,7 @@ export default function Home({ user }) {
             type="email"
             placeholder="Email"
             name="email"
-            value={userState.email}
+            value={form.email}
             onChange={(e) => changeHandler(e)}
             className="border border-black rounded p-1 my-2 w-full"
           />{" "}
@@ -140,7 +147,7 @@ export default function Home({ user }) {
             type="text"
             placeholder="Role"
             name="role"
-            value={userState.role}
+            value={form.role}
             onChange={(e) => changeHandler(e)}
             className="border border-black rounded p-1 w-full"
           />
@@ -151,6 +158,7 @@ export default function Home({ user }) {
           />
         </form>
 
+        {/* Presentation table */}
         <table class="table-auto w-full border-collapse text-sm">
           <thead>
             <tr>
@@ -164,69 +172,73 @@ export default function Home({ user }) {
             {data.length > 0 &&
               data.map((data, idx) => (
                 <Fragment key={data.id}>
-                 {!data.email ? null : ( <tr>
-                    <td
-                      className="border border-slate-100 p-4 text-slate-500 dark:text-slate-400 text-sm"
-                      contentEditable
-                      onInput={(e) =>
-                        setUserState((data) => ({
-                          ...data,
-                          name: e.target.innerText,
-                        }))
-                      }
-                    >
-                      {data.name}
-                    </td>
-                    <td
-                      className="border border-slate-100 p-4 text-slate-500 dark:text-slate-400 text-sm"
-                      contentEditable
-                      onInput={(e) =>
-                        setUserState((data) => ({
-                          ...data,
-                          email: e.target.innerText,
-                        }))
-                      }
-                    >
-                      {data.email}
-                    </td>
-                    <td
-                      className="border border-slate-100 p-4 text-slate-500 dark:text-slate-400 text-sm"
-                      contentEditable
-                      onInput={(e) =>
-                        setUserState(() => ({
-                          ...data,
-                          role: e.target.innerText,
-                        }))
-                      }
-                    >
-                      {data.role}
-                    </td>
-                    <td
-                      className="border border-slate-100 p-4 text-slate-500 dark:text-slate-400 text-sm"
-                      contentEditable
-                    >
-                      <button
-                        className="text-red-300"
-                        onClick={(e) => deleteUser(e, data.id)}
-                      >
-                        Delete
-                      </button>{" "}
-                      {" | "}
-                      <button
-                        className="text-green-400"
-                        onClick={(e) =>
-                          updateUser(e, {
-                            userId: data.id,
-                            name: data.name,
-                            email: data.email,
-                            role: data.role,
-                          })
+                  {!data.email ? null : (
+                    <tr>
+                      <td
+                        className="border border-slate-100 p-4 text-slate-500 dark:text-slate-400 text-sm"
+                        contentEditable
+                        suppressContentEditableWarning
+                        onInput={(e) =>
+                          setUpdatedData((data) => ({
+                            ...data,
+                            name: e.target.innerText,
+                          }))
                         }
                       >
-                        Update
-                      </button>
-                    </td>
-                  </tr>)}
+                        {data.name}
+                      </td>
+                      <td
+                        className="border border-slate-100 p-4 text-slate-500 dark:text-slate-400 text-sm"
+                        contentEditable
+                        suppressContentEditableWarning
+                        onInput={(e) =>
+                          setUpdatedData((data) => ({
+                            ...data,
+                            email: e.target.innerText,
+                          }))
+                        }
+                      >
+                        {data.email}
+                      </td>
+                      <td
+                        className="border border-slate-100 p-4 text-slate-500 dark:text-slate-400 text-sm"
+                        contentEditable
+                        suppressContentEditableWarning
+                        onInput={(e) =>
+                          setUpdatedData(() => ({
+                            ...data,
+                            role: e.target.innerText,
+                          }))
+                        }
+                      >
+                        {data.role}
+                      </td>
+                      <td
+                        className="border border-slate-100 p-4 text-slate-500 dark:text-slate-400 text-sm"
+                      >
+                        <button
+                          className="text-red-300"
+                          onClick={(e) => deleteUser(e, data.id)}
+                        >
+                          Delete
+                        </button>{" "}
+                        {" | "}
+                        <button
+                          className="text-green-400"
+                          onClick={(e) =>
+                            updateUser(e, {
+                              userId: updatedData.id,
+                              name:   updatedData.name,
+                              email:   updatedData.email,
+                              role:   updatedData.role,
+                            })
+                          }
+                        >
+                          Update
+                        </button>
+                      </td>
+                    </tr>
+                  )}
                 </Fragment>
               ))}
           </tbody>
