7ad907c47b1601a5ba1cf16e01d55f2607649794
romeo <romeomeshack34@gmail.com>
2023-12-22 08:43:04 +0100

    Unique email

--- src/pages/api/updateUser/[id].js
+++ src/pages/api/updateUser/[id].js
@@ -5,17 +5,19 @@ export default function handler(req, res) {
   const { name, email, role } = req.body;
 
   const user = users.find((user) => user.id === id);
+  const emailIsUnique = users.every((user) => user.email !== email);
 
-  console.log(user)
+  if (!emailIsUnique) {
+    res
+      .status(404)
+      .json({ error: "Email is not unique. Please choose a different email." });
+  }
 
   if (user) {
     user.name = name;
     user.email = email;
     user.role = role;
-
-    const updatedUser = users.map(item =>  ({...item, ...user}));
-    users.push(updatedUser);
-    res.json(updatedUser);
+    res.status(200).json(users);
   } else {
     res.status(404).json({ error: "User not found" });
   }
